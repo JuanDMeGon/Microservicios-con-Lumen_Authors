9e1e7871a449f0a569bc4d7b4452f3c0afb70c54
JuanDMeGon <juandmegon@gmail.com>
2018-10-08 16:27:05 -0500

    Creando el controlador para autores

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return authors list
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Create an instance of Author
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Return an specific author
+     * @return Illuminate\Http\Response
+     */
+    public function show($author)
+    {
+    }
+
+    /**
+     * Update thej information of an existing author
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $author)
+    {
+    }
+
+    /**
+     * Removes an existing author
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($author)
+    {
+    }
+}
